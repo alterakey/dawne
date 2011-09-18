5a9a582089a7408900cb2daf8461ecf1f5868fdb
Takahiro Yoshimura <taky@cs.monolithworks.co.jp>
2011-09-18 15:33:46 +0900

    Modelling configuration screen with PA

--- src/com/gmail/altakey/dawne/ConfigActivity.java
+++ src/com/gmail/altakey/dawne/ConfigActivity.java
@@ -1,23 +1,58 @@
 package com.gmail.altakey.dawne;
 
-import android.app.ListActivity;
+import android.preference.*;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.content.Context;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.view.View;
+import android.util.Log;
 
-public class ConfigActivity extends ListActivity
+public class ConfigActivity extends PreferenceActivity implements SharedPreferences.OnSharedPreferenceChangeListener
 {
-	@Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+	private static final String KEY_COLORTHEME = "colortheme";
+	private static final String KEY_FONTSIZE = "fontsize";
+
+	private ListPreference colortheme;
+	private ListPreference fontsize;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.config);
 
-		String[] ar = new String[] 
-			{
-				"表示色", "背景色", "フォントサイズ" 
-			};
+		this.colortheme = (ListPreference)getPreferenceScreen().findPreference(KEY_COLORTHEME);
+        this.fontsize = (ListPreference)getPreferenceScreen().findPreference(KEY_FONTSIZE);
+    }
 
-		ArrayAdapter<String> la = new ArrayAdapter<String>((Context)this, android.R.layout.simple_list_item_1, ar);
-		setListAdapter(la);
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+		SharedPreferences sharedPreferences = getPreferenceScreen().getSharedPreferences();
+		this.updateSummary(sharedPreferences, KEY_COLORTHEME);
+		this.updateSummary(sharedPreferences, KEY_FONTSIZE);
+
+        sharedPreferences.registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+		this.updateSummary(sharedPreferences, key);
+    }
+
+	private void updateSummary(SharedPreferences sharedPreferences, String key)
+	{
+        if (key.equals(KEY_COLORTHEME))
+			this.colortheme.setSummary(this.colortheme.getEntry());
+
+        if (key.equals(KEY_FONTSIZE))
+			this.fontsize.setSummary(sharedPreferences.getString(key, "") + " sp");
 	}
 }
