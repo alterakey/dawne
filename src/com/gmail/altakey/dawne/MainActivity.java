36b2d76a95a1f9c42d03c34ebdccea09529b3c27
Takahiro Yoshimura <taky@cs.monolithworks.co.jp>
2011-10-09 15:07:12 +0900

    Targeting 1.6

--- src/com/gmail/altakey/dawne/MainActivity.java
+++ src/com/gmail/altakey/dawne/MainActivity.java
@@ -88,10 +88,7 @@ public class MainActivity extends Activity
 	private void restart()
 	{
 		Intent intent = getIntent();
-		overridePendingTransition(0, 0);
-		intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
 		finish();
-		overridePendingTransition(0, 0);
 		startActivity(intent);
 	}
 
